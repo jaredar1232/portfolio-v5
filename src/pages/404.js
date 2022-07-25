0a746de26e9f4f9291054a95e8ee1fe22afbced1
jrothenberg <jrothenberg@thezebra.com>
2022-07-25 15:56:16 -0500

    update 404 page and seo name

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,39 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { SEO } from "../components/SEO"
+import { SEO } from "../components/seo"
+import styled from "styled-components"
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>404 PAGE NOT FOUND</h1>
-      <Link to="/">Go Back Home</Link>
-    </div>
+    <NotFoundSection>
+      <div className='not-found-container'>
+        <h1>404 PAGE NOT FOUND</h1>
+        <span >You look a little lost friend, let me take you&nbsp;</span>
+        <Link to="/" className="link-home">home</Link>
+      </div>
+    </NotFoundSection>
   )
 }
 
 export const Head = () => (
   <SEO />
 )
+
+const NotFoundSection = styled.section`
+  .not-found-container {
+    text-align: center;
+    font-size: 2rem;
+    color: grey;
+  }
+
+  h1 {
+    padding: 15rem 0;
+    font-size: 4rem;
+  }
+
+
+  .link-home {
+    text-decoration: none;
+    color: lightseagreen;
+  }
+`
