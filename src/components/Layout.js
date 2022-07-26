bed086b592da7c95dab9cb804baeb3fcdfee4570
jrothenberg <jrothenberg@thezebra.com>
2022-07-26 00:30:18 -0500

    change css style back

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,27 +1,19 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import Navbar from './Navbar'
 import MobileNavBar from './MobileNavBar'
 import Footer from './Footer'
-import { GlobalStyle } from '../styles'
+import '../styles/global.css'
 
 export default function Layout({ children, path }) {
 
   return (
-    <Fragment>
-      <GlobalStyle />
-
-      <div className="site">
-
-
-        <MobileNavBar />
-        <Navbar path={path} />
-
-        <main className='site-content'>
-          {children}
-        </main>
-
-        <Footer />
-      </div>
-    </Fragment>
+    <div className="site">
+      <MobileNavBar />
+      <Navbar path={path} />
+      <main className='site-content'>
+        {children}
+      </main>
+      <Footer />
+    </div>
   )
 }
