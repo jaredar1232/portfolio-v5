6e25ff45ead408cf72e645639eae9761a033f893
jaredar1232 <58494405+jaredar1232@users.noreply.github.com>
2023-11-09 12:59:00 -0800

    make navbar smaller

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -66,7 +66,7 @@ const NavWrapper = styled.nav`
   border-radius: 1rem;
   padding: 0.3rem;
 
-  font-size: 2.5rem;
+  font-size: 2rem;
   text-align: center;
 
   // lets it go over the projects/jobs
@@ -140,7 +140,7 @@ const NavWrapper = styled.nav`
   .home__logo {
     position: absolute;
     transform: translate(-50%);
-    width: 60px;
+    width: 40px;
     transition: all 0.3s;
 
     &:hover {
