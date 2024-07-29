df8a3e444b278e5360f894fab1ea77ff765a9365
jaredar1232 <58494405+jaredar1232@users.noreply.github.com>
2024-07-29 23:46:01 +0900

    adjust again

--- src/pages/index.js
+++ src/pages/index.js
@@ -95,7 +95,7 @@ const HomeSection = styled.section`
 
   &.chat-focused .header__text-box {
     transform: translateY(-50%);
-    margin-top: 550px;
+    margin-top: 60px;
 
     @media (min-width: 56.25em) {
       margin-top: 300px;
