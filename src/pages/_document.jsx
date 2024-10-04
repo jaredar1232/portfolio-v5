cbf1c8273e27469442a251fc7b3cb3ebe2ca4703
jaredar1232 <58494405+jaredar1232@users.noreply.github.com>
2024-10-04 20:00:24 +0900

    add speedinsights

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 class MyDocument extends Document {
   render() {
@@ -8,6 +9,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <SpeedInsights />
         </body>
       </Html>
     )
