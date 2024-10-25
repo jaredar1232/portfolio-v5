64b3d9e0fd0fbcd69f2f500b42d3c0795da71177
jaredar1232 <58494405+jaredar1232@users.noreply.github.com>
2024-10-25 17:05:40 +0800

    keep exit in view on modal

--- src/app/components/Modal.jsx
+++ src/app/components/Modal.jsx
@@ -34,8 +34,7 @@ export default function Modal({ modalDetails, showModal, closeModal }) {
 
       {/* Modal Content */}
       <div
-        id="modal"
-        className="relative bg-white md:p-5 rounded-lg shadow-2xl overflow-auto animate-modalEffect
+        className="relative bg-white md:p-5 rounded-lg shadow-2xl animate-modalEffect
                    w-[95%] h-3/4
                    md:w-4/5 md:h-3/5
                    lg:w-1/2 lg:h-3/5"
@@ -52,34 +51,37 @@ export default function Modal({ modalDetails, showModal, closeModal }) {
           </span>
         </button>
 
-        {/* Modal Heading */}
-        <h3 className="text-2xl md:text-xl uppercase text-center text-black pt-12 font-light">
-          {modalDetails.name}
-        </h3>
+        {/* Scrollable Content */}
+        <div id="modal" className="overflow-auto h-full pt-12">
+          {/* Modal Heading */}
+          <h3 className="text-2xl md:text-xl uppercase text-center text-black font-light">
+            {modalDetails.name}
+          </h3>
 
-        {/* Icons */}
-        <div className="flex flex-wrap justify-center items-center p-4">
-          {modalDetails.icons &&
-            modalDetails.icons.map((anIcon) => (
-              <Icons anIcon={anIcon} key={anIcon} />
-            ))}
+          {/* Icons */}
+          <div className="flex flex-wrap justify-center items-center p-4">
+            {modalDetails.icons &&
+              modalDetails.icons.map((anIcon) => (
+                <Icons anIcon={anIcon} key={anIcon} />
+              ))}
+          </div>
+
+          {/* Description */}
+          <div className="text-lg md:text-lg text-black px-8 md:px-4 py-6">
+            <b>Overview:&ensp;</b>
+            {modalDetails.description}
+          </div>
+
+          {/* Key Points */}
+          <ul className="text-sm md:text-base text-black list-none px-8 md:px-4 pb-10">
+            {modalDetails.keyPoints &&
+              modalDetails.keyPoints.map((keyPoint, index) => (
+                <li key={index} className="pt-2">
+                  &bull;&ensp;{keyPoint}
+                </li>
+              ))}
+          </ul>
         </div>
-
-        {/* Description */}
-        <div className="text-lg md:text-lg text-black px-8 md:px-4 py-6">
-          <b>Overview:&ensp;</b>
-          {modalDetails.description}
-        </div>
-
-        {/* Key Points */}
-        <ul className="text-sm md:text-base text-black list-none px-8 md:px-4 pb-10">
-          {modalDetails.keyPoints &&
-            modalDetails.keyPoints.map((keyPoint, index) => (
-              <li key={index} className="pt-2">
-                &bull;&ensp;{keyPoint}
-              </li>
-            ))}
-        </ul>
       </div>
     </div>
   );
